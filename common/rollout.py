95ca64b020d3b868ab67046854a04257ecfbe457
Xu Feng <1621322691@qq.com>
2021-03-24 00:54:41 +0800

    updated rollouts

--- common/rollout.py
+++ common/rollout.py
@@ -1,4 +1,8 @@
 from common.buffer import ReplayBuffer, TDReplayBuffer
+import numpy as np
+from common import util
+import torch
+import random
 
 def rollout(env, agent, max_env_steps, max_trajectories=-1, max_traj_length=1000, n=1, gamma=0.99):
     #max_env_steps: max environments to sample
@@ -8,12 +12,21 @@ def rollout(env, agent, max_env_steps, max_trajectories=-1, max_traj_length=1000
     max_rollout_buffer_size = max_env_steps + max_traj_length # in case an additional full trajectory is sampled
     if n == 1:
         #naive buffer case
-        rollout_buffer = ReplayBuffer(env.observation_space, env.action_space, max_buffer_size=max_rollout_buffer_size)
+        rollout_buffer = NaiveRollout(env.observation_space, env.action_space, max_buffer_size=max_rollout_buffer_size)
     else:
-        rollout_buffer = TDReplayBuffer(env.observation_space, env.action_space, n=n, gamma=gamma, max_buffer_size=max_rollout_buffer_size)
-    for i in range(max_trajectories):
+        #td buffer case
+        rollout_buffer = TDRollout(env.observation_space, env.action_space, n=n, gamma=gamma, max_buffer_size=max_rollout_buffer_size)
+    if max_trajectories == -1:
+        max_trajectories = np.inf
+    tot_env_steps = 0
+    tot_trajectories = 0
+    while(tot_trajectories < max_trajectories):
         states, actions, next_states, rewards, dones = rollout_trajectory(env, agent, max_traj_length)
         rollout_buffer.add_traj(states, actions, next_states, rewards, dones)
+        tot_env_steps += len(states)
+        if tot_env_steps > max_env_steps:
+            break
+        tot_trajectories += 1
     return rollout_buffer
 
 
@@ -37,3 +50,139 @@ def rollout_trajectory(env, agent, max_traj_length):
         if done:
             break
     return states, actions, next_states, rewards, dones
+
+
+
+class NaiveRollout(object):
+    def __init__(self, obs_space, action_space, max_buffer_size = 1000000, action_type = gym.spaces.discrete.Discrete, **kwargs):
+        self.max_buffer_size = max_buffer_size
+        self.curr = 0
+        obs_dim = obs_space.shape[0]
+        action_dim = action_space.shape[0]
+        self.obs_buffer = np.zeros((max_buffer_size, obs_dim))
+        self.action_buffer = np.zeros((max_buffer_size, action_dim))
+        self.next_obs_buffer = np.zeros((max_buffer_size,obs_dim))
+        self.reward_buffer = np.zeros((max_buffer_size,))
+        self.done_buffer = np.zeros((max_buffer_size,))
+        self.max_sample_size = 0
+        self.action_type = action_type
+
+    def add_traj(self, obs_list, action_list, next_obs_list, reward_list, done_list):
+        for obs, action, next_obs, reward, done in zip(obs_list, action_list, next_obs_list, reward_list, done_list):
+            self.add_tuple(obs, action, next_obs, reward, done)
+    
+    def add_tuple(self, obs, action, next_obs, reward, done):
+        self.obs_buffer[self.curr] = obs
+        self.action_buffer[self.curr] = action
+        self.next_obs_buffer[self.curr] = next_obs
+        self.reward_buffer[self.curr] = reward
+        self.done_buffer[self.curr] = done
+        self.curr = (self.curr+1) % self.max_buffer_size
+        self.max_sample_size = min(self.max_sample_size+1, self.max_buffer_size)
+
+    def finalize(self):
+        #convert to tensor and pass data to device
+        self.obs_buffer = torch.FloatTensor(self.obs_buffer[:self.max_sample_size]).to(util.device)
+        self.action_buffer = torch.FloatTensor(self.action_buffer[:self.max_sample_size]).to(util.device)
+        self.next_obs_buffer = torch.FloatTensor(self.next_obs_buffer[:self.max_sample_size]).to(util.device)
+        self.reward_buffer = torch.FloatTensor(self.reward_buffer[:self.max_sample_size]).to(util.device).unsqueeze(1)
+        self.done_buffer = torch.FloatTensor(self.done_buffer[:self.max_sample_size]).to(util.device).unsqueeze(1)
+
+
+    def sample_batch(self, batch_size, to_tensor = True, step_size: int = 1):
+        # batch_size: 
+        # to_tensor: if convert to torch.tensor type as pass to util.device
+        # step_size: return a list of next states, returns and dones with size n
+        batch_size = min(self.max_sample_size, batch_size)
+        index = random.sample(range(self.max_sample_size), batch_size)
+        obs_batch, action_batch, next_obs_batch, reward_batch, done_batch = self.obs_buffer[index], \
+            self.action_buffer[index],\
+            self.next_obs_buffer[index],\
+            self.reward_buffer[index],\
+            self.done_buffer[index]
+        return obs_batch, action_batch, next_obs_batch, reward_batch, done_batch
+
+class TDRollout(object):
+    def __init__(self, obs_space, action_space, n, gamma, max_buffer_size = 1000000, **kwargs):
+        self.n = n # parameter for td(n)
+        self.max_buffer_size = max_buffer_size
+        self.gamma = gamma
+        obs_dim = obs_space.shape[0]
+        action_dim = action_space.shape[0]
+        self.obs_buffer = np.zeros((max_buffer_size, obs_dim))
+        self.action_buffer = np.zeros((max_buffer_size, action_dim))
+        #self.next_obs_buffer = np.zeros((max_buffer_size,obs_dim))
+        #self.reward_buffer = np.zeros((max_buffer_size,))
+        self.done_buffer = np.ones((max_buffer_size,))
+        self.n_step_obs_buffer = np.zeros((max_buffer_size,obs_dim))
+        self.discounted_reward_buffer = np.zeros((max_buffer_size,))
+        self.n_step_done_buffer = np.zeros(max_buffer_size,)
+        #insert a random state at initialization to avoid bugs when inserting the first state
+        self.max_sample_size = 1
+        self.curr = 1
+
+    def add_traj(self, obs_list, action_list, next_obs_list, reward_list, done_list):
+        for obs, action, next_obs, reward, done in zip(obs_list, action_list, next_obs_list, reward_list, done_list):
+            self.add_tuple(obs, action, next_obs, reward, done)
+    
+    def add_tuple(self, obs, action, next_obs, reward, done):
+        # store to instant memories
+        self.obs_buffer[self.curr] = obs
+        self.action_buffer[self.curr] = action
+        #self.next_obs_buffer[self.curr] = next_obs
+        #self.reward_buffer[self.curr] = reward
+        self.done_buffer[self.curr] = done
+        #store precalculated tn(n) info
+        self.n_step_obs_buffer[self.curr] = next_obs
+        self.discounted_reward_buffer[self.curr] = reward
+        self.n_step_done_buffer[self.curr] = 1.
+        breaked = False # record if hit the previous trajectory
+        for i in range(self.n - 1):
+            idx = (self.curr - i - 1) % self.max_sample_size # use max sample size cuz the buffer might not have been full
+            if self.done_buffer[idx]: # hit the previous trajecory, break
+                breaked = True
+                break
+            self.discounted_reward_buffer[idx] += (self.gamma ** (i + 1))  * reward
+        if not breaked  and not self.done_buffer[(self.curr - self.n) % self.max_sample_size]:# not hit last trajctory, set the n-step-next state for the last state
+            self.n_step_obs_buffer[(self.curr - self.n) % self.max_sample_size] = obs
+        if done:#set the n-step-next-obs of the previous n states to the current state
+            for i in range(self.n - 1):
+                idx = (self.curr - i - 1) % self.max_sample_size
+                if self.done_buffer[idx]:# hit the last trajectory
+                    break
+                self.n_step_obs_buffer[idx] = next_obs
+                self.n_step_done_buffer[idx] = 1.0
+        else:
+            for i in range(self.n - 1):
+                idx = (self.curr - i - 1) % self.max_sample_size
+                if self.done_buffer[idx]:# hit the last trajectory
+                    break
+                self.n_step_obs_buffer[idx] = next_obs
+                self.n_step_done_buffer[idx] = 0.0
+
+        # another special case is that n > max_sample_size, that might casuse a cyclic visiting of a buffer that has no done states
+        # this has been avoided by setting initializing all done states to true
+        self.curr = (self.curr+1) % self.max_buffer_size
+        self.max_sample_size = min(self.max_sample_size + 1, self.max_buffer_size)
+
+
+    def finalize(self):
+        #convert to tensor and pass data to device
+        self.obs_buffer = torch.FloatTensor(self.obs_buffer[:self.max_sample_size]).to(util.device)
+        self.action_buffer = torch.FloatTensor(self.action_buffer[:self.max_sample_size]).to(util.device)
+        self.n_step_obs_buffer = torch.FloatTensor(self.n_step_obs_buffer[:self.max_sample_size]).to(util.device)
+        self.discounted_reward_buffer = torch.FloatTensor(self.discounted_reward_buffer[:self.max_sample_size]).to(util.device).unsqueeze(1)
+        self.n_step_done_buffer = torch.FloatTensor(self.n_step_done_buffer[:self.max_sample_size]).to(util.device).unsqueeze(1)
+
+    def sample_batch(self, batch_size, to_tensor = True):
+        batch_size = min(self.max_sample_size, batch_size)
+        index = random.sample(range(self.max_sample_size), batch_size)
+        #obs_batch, action_batch, next_obs_batch, reward_batch, n_step_obs_batch, discounted_reward_batch, done_batch, n_step_done_batch =\
+        obs_batch, action_batch, n_step_obs_batch, discounted_reward_batch, n_step_done_batch =\
+            self.obs_buffer[index], \
+            self.action_buffer[index],\
+            self.n_step_obs_buffer[index],\
+            self.discounted_reward_buffer[index],\
+            self.n_step_done_buffer[index]
+        return obs_batch, action_batch, n_step_obs_batch, discounted_reward_batch, n_step_done_batch
+
---
+++ ppo/main.py
@@ -0,0 +1,66 @@
+import os
+import gym
+import click
+from common.logger import Logger
+from ppo.trainer import PPOTrainer
+from ppo.model import PPOAgent
+from common.util import set_device, update_parameters, load_config
+from common.buffer import ReplayBuffer
+from sac.wrapper import SACWrapper
+from  common import util
+
+@click.command()
+@click.argument("config-path",type=str)
+@click.option("--log-dir", default="logs")
+@click.option("--gpu", type=int, default=-1)
+@click.option("--print-log", type=bool, default=True)
+@click.option("--info", type=str, default="")
+@click.argument('args', nargs=-1)
+def main(config_path, log_dir, gpu, print_log,info, args):
+    #load args
+    args = load_config(config_path, args)
+    #initialize device
+    set_device(gpu)
+    #initialize logger
+    env_name = args['env_name']
+    logger = Logger(log_dir, prefix = env_name+"-"+info, print_to_terminal=print_log)
+    logger.log_str("logging to {}".format(logger.log_path))
+
+    #save args
+    logger.log_str_object("parameters", log_dict = args)
+
+    #initialize environment
+    logger.log_str("Initializing Environment")
+    env = gym.make(env_name)
+    env = SACWrapper(env, **args['env'])
+    eval_env = gym.make(env_name)
+    eval_env = SACWrapper(eval_env, **args['env'])
+    state_space = env.observation_space
+    action_space = env.action_space
+
+    #initialize buffer
+    logger.log_str("Initializing Buffer")
+    buffer = ReplayBuffer(state_space, action_space, **args['buffer'])
+
+    #initialize agent
+    logger.log_str("Initializing Agent")
+    agent = PPOAgent(state_space, action_space, **args['agent'])
+
+    #initialize trainer
+    logger.log_str("Initializing Trainer")
+    trainer  = PPOTrainer(
+        agent,
+        env,
+        eval_env,
+        buffer,
+        logger,
+        **args['trainer']
+    )
+
+    
+    logger.log_str("Started training")
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
