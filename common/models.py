5f16d1299d2e7a1366ead59b151be647b58e533c
Xu Feng <1621322691@qq.com>
2021-03-31 00:20:42 +0800

    added random agent for testing

--- common/models.py
+++ common/models.py
@@ -18,3 +18,25 @@ class BaseAgent(object):
     @abstractmethod
     def save_model(self, target_dir, ite):
         pass
+
+
+
+class RandomAgent(BaseAgent):
+    def __init__(self,observation_space, action_space, **kwargs):
+        self.observation_space = observation_space
+        self.action_space = action_space
+
+    def update(self,data_batch, **kwargs):
+        return
+
+
+    def select_action(self, state, **kwargs):
+        return self.action_space.sample()
+
+
+    def load_model(self, dir, **kwargs):
+        pass
+    
+
+    def save_model(self, target_dir, ite, **kwargs):
+        pass
