bf9a5fe98ea88bba14034e8ddf8f0884ef6d308c
Xu Feng <1621322691@qq.com>
2021-03-31 13:17:37 +0800

    fixed atanh bug

--- common/networks.py
+++ common/networks.py
@@ -159,7 +159,9 @@ class GaussianPolicyNetwork(nn.Module):
         #to reperameterize, use rsample
         #also return the new actions for ppo
         de_scaled_actions = (actions - self.action_bias) / self.action_scale
-        de_scaled_actions = torch.arctanh(de_scaled_actions)
+        #apply arcranh, if has torch 1.8, use torch.atanh function
+        #atanh(x) = 1/2*log [(1+x)/(1-x)]
+        de_scaled_actions = 0/5 * torch.log( (1 + de_scaled_actions) / (1- de_scaled_actions))
         old_log_pi = dist.log_prob(de_scaled_actions).sum(1, keepdim=True)
         dist_entropy = dist.entropy()
         #
