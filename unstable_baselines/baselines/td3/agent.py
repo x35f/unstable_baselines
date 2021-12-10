2941b10cacfd750e4ec1f20a4ee5b875d6ef91b5
x35f <1621322691@qq.com>
2021-12-10 21:36:01 +0800

    add td3

---
+++ unstable_baselines/baselines/td3/agent.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn.functional as F
+import gym 
+import os
+from torch import nn
+from unstable_baselines.common.agents import BaseAgent
+from unstable_baselines.common.networks import MLPNetwork, PolicyNetworkFactory, get_optimizer
+from unstable_baselines.common.buffer import ReplayBuffer
+import numpy as np
+from unstable_baselines.common import util
+
+class DDPGAgent(torch.nn.Module, BaseAgent):
+    def __init__(self,observation_space, action_space,
+        target_action_noise=0.2,
+        noise_range=0.1,
+        target_smoothing_tau=0.1,
+        **kwargs):
+        obs_dim = observation_space.shape[0]
+        action_dim = action_space.shape[0]
+        super(DDPGAgent, self).__init__()
+        #save parameters
+        self.args = kwargs
+
+        # get per flag
+        self.per = self.args.get('per', False)
+
+        #initilze networks
+        self.q1_network = MLPNetwork(obs_dim + action_dim, 1, **kwargs['q_network'])
+        self.q2_network = MLPNetwork(obs_dim + action_dim, 1, **kwargs['q_network'])
+        self.policy_network = PolicyNetworkFactory.get(obs_dim, action_space,  ** kwargs['policy_network'])
+        self.target_policy_network = PolicyNetworkFactory.get(obs_dim, action_space,  ** kwargs['policy_network'])
+
+        #sync network parameters
+        util.soft_update_network(self.q1_network, self.target_q_network, 1.0)
+        util.soft_update_network(self.policy_network, self.target_policy_network, 1.0)
+
+        #pass to util.util.device
+        self.q1_network = self.q1_network.to(util.device)
+        self.q2_network = self.q2_network.to(util.device)
+        self.policy_network = self.policy_network.to(util.device)
+        self.target_policy_network = self.target_policy_network.to(util.device)
+        
+        #register networks
+        self.networks = {
+            'q1_network': self.q1_network,
+            'q2_network': self.q2_network,
+            'policy_network': self.policy_network,
+            'target_policy_network': self.target_policy_network
+        }
+
+        #initialize optimizer
+        self.q_optimizer = get_optimizer(kwargs['q_network']['optimizer_class'], self.q_network, kwargs['q_network']['learning_rate'])
+        self.policy_optimizer = get_optimizer(kwargs['policy_network']['optimizer_class'], self.policy_network, kwargs['policy_network']['learning_rate'])
+
+        #hyper-parameters
+        self.gamma = kwargs['gamma']
+        self.action_upper_cound = action_space.high[0]
+        self.action_lower_cound = action_space.low[0]
+        self.target_action_noise = target_action_noise
+        self.noise_range = noise_range
+        self.target_smoothing_tau = target_smoothing_tau
+
+
+    def update(self, data_batch, update_policy_network=False):
+        obs_batch = data_batch['obs']
+        action_batch = data_batch['action'] 
+        next_obs_batch = data_batch['next_obs'] 
+        reward_batch = data_batch['reward']
+        done_batch = data_batch['done']
+        
+        curr_state_q1_value = self.q1_network(torch.cat([obs_batch, action_batch], dim=1))
+        curr_state_q2_value = self.q2_network(torch.cat([obs_batch, action_batch], dim=1))
+        
+        #get new action output
+        curr_state_action_info = self.policy_network.sample(obs_batch)
+        new_curr_state_action = curr_state_action_info['action_scaled']
+        next_state_action_info = self.target_policy_network.sample(next_obs_batch)
+        next_state_action = next_state_action_info['action_scaled']
+
+        #apply noise to next state_action
+        epsilon = torch.randn_like(next_state_action) * self.target_action_noise
+        epsilon = torch.clamp(epsilon, -self.noise_range, self.noise_range)
+        next_state_action = next_state_action + epsilon
+        next_state_action = torch.clmap(next_state_action, self.action_lower_bound, self.action_upper_bound)
+
+        #compute target value
+
+        next_state_q1_value = self.q1_network(torch.cat([next_obs_batch, next_state_action], dim=1))
+        next_state_q2_value = self.q2_network(torch.cat([next_obs_batch, next_state_action], dim=1))
+        next_state_min_q = torch.min(next_state_q1_value, next_state_q2_value)
+        target_q = reward_batch + self.gamma * (1. - done_batch) * next_state_min_q
+
+
+        #compute q loss
+        q1_loss = F.mse_loss(curr_state_q1_value, target_q.detach())
+        q2_loss = F.mse_loss(curr_state_q2_value, target_q.detach())
+        q_loss = q1_loss + q2_loss
+        q_loss_val = q_loss.item()
+        q1_loss_val = q1_loss.item()
+        q2_loss_val = q2_loss.item()
+        
+        self.q_optimizer.zero_grad()
+        q_loss.backward()
+        self.q_optimizer.step()
+
+        if update_policy_network:
+            #compute policy loss
+            new_curr_state_q_value = self.q_network(torch.cat([obs_batch, new_curr_state_action], dim=1))
+            policy_loss = - new_curr_state_q_value.mean()
+            policy_loss_value = policy_loss.item()
+            self.policy_optimizer.zero_grad()
+            policy_loss.backward()
+
+            self.policy_optimizer.step()
+
+            #update target network
+            self.update_target_network()
+
+        return {
+            "loss/q1": q1_loss_val, 
+            "loss/q2": q2_loss_val, 
+            "loss/q": q_loss_val, 
+            "loss/policy": policy_loss_value, 
+        }
+        
+
+    def update_target_network(self):
+        util.soft_update_network(self.q_network, self.target_q_network, self.target_smoothing_tau)
+        util.soft_update_network(self.policy_network, self.target_policy_network, self.target_smoothing_tau)
+            
+    def select_action(self, state):
+        if type(state) != torch.tensor:
+            state = torch.FloatTensor(np.array([state])).to(util.device)
+        with torch.no_grad():
+            action_info = self.policy_network.sample(state)
+        action = action_info['action_scaled']
+        log_std = action_info.get("log_prob", 1)
+        return action.cpu().numpy()[0], log_std
+
+
+        
+
+
+
---
+++ unstable_baselines/baselines/td3/trainer.py
@@ -0,0 +1,152 @@
+from unstable_baselines.common import util
+from unstable_baselines.common.trainer import BaseTrainer
+import numpy as np
+from tqdm import tqdm
+from time import time
+import os
+from tqdm import  tqdm
+
+import warnings
+
+class DDPGTrainer(BaseTrainer):
+    def __init__(self, agent, env, eval_env, buffer,
+            batch_size=32,
+            policy_delay=2,
+            max_trajectory_length=1000,
+            eval_interval=10,
+            num_eval_trajectories=5,
+            max_iteration=100000,
+            save_model_interval=10000,
+            random_sample_timestep=10000,
+            start_update_timestep=2000,
+            update_interval=50,
+            save_video_demo_interval=10000,
+            log_interval=100,
+            load_dir="",
+            action_noise_scale = 0.1,
+            **kwargs):
+        warnings.warn('redundant arguments for trainer: {}'.format(kwargs))
+        self.agent = agent
+        self.buffer = buffer
+        self.env = env 
+        self.eval_env = eval_env
+        #hyperparameters
+        self.action_upper_bound  = env.action_space.high[0]
+        self.action_lower_bound  = env.action_space.low[0]
+        self.batch_size = batch_size
+        self.policy_delay = policy_delay
+        self.max_trajectory_length = max_trajectory_length
+        self.eval_interval = eval_interval
+        self.num_eval_trajectories = num_eval_trajectories
+        self.max_iteration = max_iteration
+        self.save_model_interval = save_model_interval
+        self.save_video_demo_interval = save_video_demo_interval
+        self.random_sample_timestep = random_sample_timestep
+        self.start_update_timestep = start_update_timestep
+        self.update_interval = update_interval
+        self.log_interval = log_interval
+        self.action_noise_scale = action_noise_scale
+        if load_dir != "" and os.path.exists(load_dir):
+            self.agent.load(load_dir)
+
+    def train(self):
+        train_traj_rewards = [0]
+        train_traj_lengths = [0]
+        iteration_durations = []
+        traj_reward = 0
+        traj_length = 0
+        done = False
+        obs = self.env.reset()
+        for ite in tqdm(range(self.max_iteration)): # if system is windows, add ascii=True to tqdm parameters to avoid powershell bugs
+            iteration_start_time = time()
+            
+            if ite < self.random_sample_timestep:
+                action = self.env.action_space.sample()
+            else:
+                action, _ = self.agent.select_action(obs)
+                # add noise and clip action
+                action = action + np.random.normal(size = action.shape, scale=self.action_noise_scale)
+                action = np.clip(action, self.action_lower_bound, self.action_upper_bound)
+            next_obs, reward, done, _ = self.env.step(action)
+            traj_length  += 1
+            traj_reward += reward
+            if traj_length >= self.max_trajectory_length:
+                done = True
+            self.buffer.add_tuple(obs, action, next_obs, reward, float(done))
+            obs = next_obs
+            if done or traj_length >= self.max_trajectory_length - 1:
+                obs = self.env.reset()
+                train_traj_rewards.append(traj_reward)
+                train_traj_lengths.append(traj_length)
+                traj_length = 0
+                traj_reward = 0
+            if ite < self.start_update_timestep or ite % self.update_interval != 0:
+                continue
+            
+            for i in range(self.update_interval):
+                update_policy_network = i % self.policy_delay == 0
+                data_batch = self.buffer.sample_batch(self.batch_size)
+                loss_dict = self.agent.update(data_batch, update_policy_network=update_policy_network)
+           
+            iteration_end_time = time()
+            iteration_duration = iteration_end_time - iteration_start_time
+            iteration_durations.append(iteration_duration)
+
+            if ite % self.log_interval == 0:
+                util.logger.log_var("return/train",traj_reward, ite)
+                util.logger.log_var("length/train",traj_length, ite)
+                for loss_name in loss_dict:
+                    util.logger.log_var(loss_name, loss_dict[loss_name], ite)
+                util.logger.log_var("time/train_iteration_duration(s)", np.mean(iteration_durations[-50:]), ite)
+
+            if ite % self.eval_interval == 0:
+                eval_start_time = time()
+                log_dict = self.eval()
+                eval_duration = time() - eval_start_time
+                avg_eval_reward = log_dict['return/eval']
+                for log_key in log_dict:
+                    util.logger.log_var(log_key, log_dict[log_key], ite)
+                util.logger.log_var('time/evaluation_duration(s)', eval_duration, ite)
+                remaining_seconds = int((self.max_iteration - ite + 1) / self.update_interval * np.mean(iteration_durations[-50:]))
+                time_remaining_str = util.second_to_time_str(remaining_seconds)
+                summary_str = "iteration {}/{}:\ttrain return {:.02f}\teval return {:02f}\teta: {}".format(ite, self.max_iteration, train_traj_rewards[-1],avg_eval_reward,time_remaining_str)
+                util.logger.log_str(summary_str)
+
+            if ite % self.save_model_interval == 0:
+                self.agent.save_model(ite)
+
+            if ite % self.save_video_demo_interval == 0:
+                self.save_video_demo(ite)
+
+
+    def eval(self):
+        rewards = []
+        lengths = []
+        for episode in range(self.num_eval_trajectories):
+            traj_reward = 0
+            traj_length = 0
+            obs = self.eval_env.reset()
+            for step in range(self.max_trajectory_length):
+                action, _ = self.agent.select_action(obs)
+                next_obs, reward, done, _ = self.eval_env.step(action)
+                traj_reward += reward
+                obs = next_obs
+                traj_length += 1 
+                if done:
+                    break
+            lengths.append(traj_length)
+            traj_reward /= self.eval_env.reward_scale
+            rewards.append(traj_reward)
+        return {
+            "return/eval": np.mean(rewards),
+            "length/eval": np.mean(lengths)
+        }
+
+    
+
+
+
+
+            
+
+
